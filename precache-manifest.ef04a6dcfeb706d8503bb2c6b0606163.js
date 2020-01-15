6233aadb72df165458d41c50686f6cdff7ef3844
gregkelley <suxor@buttoncat.org>
2020-01-15 13:16:48 -0700

    Updates

---
+++ precache-manifest.ef04a6dcfeb706d8503bb2c6b0606163.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c84a031f0c342a48fdff07c4d973564",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "33cb60e0a9abb7c9dcae",
+    "url": "/robot/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d9ced0c4ba07f72cbbe1",
+    "url": "/robot/static/css/main.4b3e5cf3.chunk.css"
+  },
+  {
+    "revision": "33cb60e0a9abb7c9dcae",
+    "url": "/robot/static/js/2.549c689d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robot/static/js/2.549c689d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9ced0c4ba07f72cbbe1",
+    "url": "/robot/static/js/main.3ccbe77e.chunk.js"
+  },
+  {
+    "revision": "33864a2b2e0ac67d33fb",
+    "url": "/robot/static/js/runtime-main.35336c78.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
